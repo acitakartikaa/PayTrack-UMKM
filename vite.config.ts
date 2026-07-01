4a8e020b50b6f247bcbb058f57a437167ad59f53
acitakartikaa <acitakartikaa@gmail.com>
2026-07-01 21:36:32 +0700

    build: configure single-file output for production
    Integrated vite-plugin-single-file to bundle the application into a single HTML file and updated build scripts to ensure proper asset management.

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,19 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig} from 'vite';
+import {viteSingleFile} from 'vite-plugin-singlefile';
 
 export default defineConfig(() => {
   return {
-    plugins: [react(), tailwindcss()],
+    plugins: [react(), tailwindcss(), viteSingleFile()],
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
+      },
+    },
+    build: {
+      rollupOptions: {
+        input: path.resolve(__dirname, 'src/index.html'),
       },
     },
     server: {
